1dd7475104a92c010c076570a3208b3791c3f565
Tolulope Popoola <tolulopeontop@gmail.com>
2019-01-11 23:06:07 +0100

    Server fixed

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-vertical-80854',
-      user : 'postgres',
-      password : 'qwertyui',
-      database : 'smart-brain'
+      connectionString : 'process.env.DATABASE_URL',
+      ssl: true,
     }
   });
 
@@ -34,6 +32,6 @@ app.put('/image', (req,res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req,res) => { image.handleApiCall(req, res)})
 
 
-app.listen(process.env.PORT || 3000, ()=> {
-    console.log(`App is running on port ${process.env.PORT}`);
+app.listen(3000, ()=> {
+    console.log('App is running on port 3000');
 })
