0e245c6cc374202af0025bff6cf20912e6191047
Tolulope Popoola <tolulopeontop@gmail.com>
2019-01-12 10:46:12 +0100

    Image link updated

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: '75df2b4fff83427d9d80f2b62e100ec5'
+    apiKey: process.env.API_CLARIFAI
    });
 
 const handleApiCall = (req, res) => {
